dca6a568cafd4542ac694aa18c08b08fe1c757ea
errnafeb <errnafeb@gmail.com>
2021-10-11 14:24:28 +0700

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,90 +1,65 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+  public function me() {
+    return ['NIS' => 3103119058,
+        'name' => 'Erna febriyanti',
+        'gender' => 'Female',
+        'phone' => '083104158928',
+        'class' => 'XII RPL 2'];
+  }
+  // R E G I S T E R
+  public function register(Request $request) {
+    $fields = $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|string|unique:users,email',
+        'password' => 'required|string|confirmed'
+    ]);
+    $user = User::create([
+        'name' => $fields['name'],
+        'email' => $fields['email'],
+        'password' => bcrypt($fields['password'])
+    ]);
+    $token = $user->createToken('myapptoken')->plainTextToken;
+    $response = [
+        'user' => $user,
+        'token' => $token
+    ];
+    return response($response, 201);
+  }
+
+  // L O G I N
+  public function login(Request $request) {
+      $fields = $request->validate([
+          'email' => 'required|string',
+          'password' => 'required|string'
+      ]);
+      $user = User::where('email', $fields['email'])->first();
+      if(!$user || !Hash::check($fields['password'], $user->password)) {
+          return response([
+              'message' => 'Bad creds'
+          ], 401);
+      }
+      $token = $user->createToken('myapptoken')->plainTextToken;
+      $response = [
+          'user' => $user,
+          'token' => $token
+      ];
+      return response($response, 201);
+  }
+
+  // L O G O U T
+  public function logout(Request $request)
     {
-        return[
-            'NIS' => '3103119058',
-            'Name' => 'Erna febriyanti',
-            'Gender' => 'Perempuan',
-            'Phone' => '083104158928',
-            'Class' => 'XII RPL 2',
+        $request->user()->currentAccessToken()->delete();
+        return [
+            'message' => 'Logged out'
         ];
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
